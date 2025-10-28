3aec2e73e7f5213aa9750dd9ab677d9344e0c560
Eric <eric@mindbrew.com>
2025-10-28 14:25:36 -0600

    Register SendOnceEmailRepository via EntityManager
    Configured SendOnceEmailRepository to be registered using Doctrine's entity manager factory for proper repository instantiation. Updated service loading to reflect this change and improved repository registration for the MauticSendOnceBundle.

--- Config/services.php
+++ Config/services.php
@@ -3,7 +3,10 @@
 declare(strict_types=1);
 
 use Mautic\CoreBundle\DependencyInjection\MauticCoreExtension;
+use MauticPlugin\MauticSendOnceBundle\Entity\SendOnceEmail;
+use MauticPlugin\MauticSendOnceBundle\Entity\SendOnceEmailRepository;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
 
 return function (ContainerConfigurator $configurator): void {
     $services = $configurator->services()
@@ -18,7 +21,12 @@ return function (ContainerConfigurator $configurator): void {
     $services->load('MauticPlugin\\MauticSendOnceBundle\\', '../')
         ->exclude('../{'.implode(',', array_merge(MauticCoreExtension::DEFAULT_EXCLUDES, $excludes)).'}');
 
-    $services->load('MauticPlugin\\MauticSendOnceBundle\\Entity\\', '../Entity/*Repository.php');
+    // Register repository properly through EntityManager
+    $services->set(SendOnceEmailRepository::class)
+        ->factory([service('doctrine.orm.entity_manager'), 'getRepository'])
+        ->args([SendOnceEmail::class]);
+
+    $services->load('MauticPlugin\\MauticSendOnceBundle\\Entity\\', '../Entity/EmailSendRecordRepository.php');
 
     // Decorate EmailType to add send_once checkbox
     $services->set(MauticPlugin\MauticSendOnceBundle\Form\Type\OverrideEmailType::class)
