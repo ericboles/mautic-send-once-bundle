408fb0b2155a6b5e53218a7022651a25e28c3a80
Eric <eric@mindbrew.com>
2025-10-28 12:27:03 -0600

    Update base class for SendOnceBundle
    Changed the base class of MauticSendOnceBundle from AbstractPluginBundle to PluginBundleBase to align with updated plugin architecture.

--- MauticSendOnceBundle.php
+++ MauticSendOnceBundle.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace MauticPlugin\MauticSendOnceBundle;
 
-use Mautic\IntegrationsBundle\Bundle\AbstractPluginBundle;
+use Mautic\PluginBundle\Bundle\PluginBundleBase;
 
 /**
  * Send Once Bundle
@@ -12,6 +12,6 @@ use Mautic\IntegrationsBundle\Bundle\AbstractPluginBundle;
  * Ensures segment emails can only be sent once, then automatically unpublishes them.
  * If accidentally re-activated, prevents any sends (protects against sending to new segment members).
  */
-class MauticSendOnceBundle extends AbstractPluginBundle
+class MauticSendOnceBundle extends PluginBundleBase
 {
 }
