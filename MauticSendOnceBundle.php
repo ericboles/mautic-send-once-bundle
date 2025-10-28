aae2fb4e8c9f2c9cd8f17affde3e435e434697a7
Eric <eric@mindbrew.com>
2025-10-28 12:46:39 -0600

    Add bundle extension and update base class
    Introduced MauticSendOnceExtension for dependency injection and switched MauticSendOnceBundle to extend AbstractPluginBundle instead of PluginBundleBase for improved integration.

--- MauticSendOnceBundle.php
+++ MauticSendOnceBundle.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace MauticPlugin\MauticSendOnceBundle;
 
-use Mautic\PluginBundle\Bundle\PluginBundleBase;
+use Mautic\IntegrationsBundle\Bundle\AbstractPluginBundle;
 
 /**
  * Send Once Bundle
@@ -12,6 +12,6 @@ use Mautic\PluginBundle\Bundle\PluginBundleBase;
  * Ensures segment emails can only be sent once, then automatically unpublishes them.
  * If accidentally re-activated, prevents any sends (protects against sending to new segment members).
  */
-class MauticSendOnceBundle extends PluginBundleBase
+class MauticSendOnceBundle extends AbstractPluginBundle
 {
 }
